b9bac6507dbaaa66b79a58d5894a24787aa4e117
lrli <nightmeng@gmail.com>
2025-09-02 11:15:22 +0800

    support self defines images by env

--- internal/controller/arksapplication_controller.go
+++ internal/controller/arksapplication_controller.go
@@ -19,6 +19,7 @@ package controller
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 	"time"
 
@@ -363,7 +364,7 @@ func (r *ArksApplicationReconciler) reconcile(ctx context.Context, application *
 }
 
 func generateLws(application *arksv1.ArksApplication, model *arksv1.ArksModel) (*lwsapi.LeaderWorkerSet, error) {
-	image, err := getApplicationImage(application)
+	image, err := getApplicationRuntimeImage(application)
 	if err != nil {
 		return nil, err
 	}
@@ -487,6 +488,7 @@ func generateLws(application *arksv1.ArksApplication, model *arksv1.ArksModel) (
 						Affinity:           application.Spec.InstanceSpec.Affinity,
 						NodeSelector:       application.Spec.InstanceSpec.NodeSelector,
 						Tolerations:        application.Spec.InstanceSpec.Tolerations,
+						ImagePullSecrets:   application.Spec.RuntimeImagePullSecrets,
 						Containers: []corev1.Container{
 							{
 								Name:         "worker",
@@ -523,7 +525,7 @@ func generateLwsLabels(application *arksv1.ArksApplication, role string) map[str
 	return podLabels
 }
 
-func getApplicationImage(application *arksv1.ArksApplication) (string, error) {
+func getApplicationRuntimeImage(application *arksv1.ArksApplication) (string, error) {
 	// Make sure the Runtime match with the RuntimeImage.
 	if application.Spec.RuntimeImage != "" {
 		return application.Spec.RuntimeImage, nil
@@ -531,10 +533,22 @@ func getApplicationImage(application *arksv1.ArksApplication) (string, error) {
 
 	switch application.Spec.Runtime {
 	case string(arksv1.ArksRuntimeVLLM):
+		vllmImage := os.Getenv("ARKS_RUNTIME_DEFAULT_VLLM_IMAGE")
+		if vllmImage != "" {
+			return vllmImage, nil
+		}
 		return "vllm/vllm-openai:v0.8.2", nil
 	case string(arksv1.ArksRuntimeSGLang):
+		sglangImage := os.Getenv("ARKS_RUNTIME_DEFAULT_SGLANG_IMAGE")
+		if sglangImage != "" {
+			return sglangImage, nil
+		}
 		return "lmsysorg/sglang:v0.4.5-cu124", nil
 	case string(arksv1.ArksRuntimeDynamo):
+		dynamoImage := os.Getenv("ARKS_RUNTIME_DEFAULT_DYNAMO_IMAGE")
+		if dynamoImage != "" {
+			return dynamoImage, nil
+		}
 		return "scitixai/k8s/dynamo:vllm", nil
 		// return "docker.io/scitixai/dynamo:vllm", nil
 	default:
